9e5e61f94e019349d12e350651a6c8a169c75044
griimx <soumikpradhan14@gmail.com>
2017-12-20 00:52:13 +0530

    session prototype and db start

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,7 @@
 var Nerd = require('./models/nerd');
 
     module.exports = function(app) {
-        // route to handle creating goes here (app.post)
-        // route to handle delete goes here (app.delete)
-
-        // frontend routes =========================================================
-        // route to handle all angular requests
+        
         app.get('/', function(req, res) {
             res.sendfile('./public/views/home.html'); // load our public/index.html file
         });
@@ -18,7 +14,7 @@ var Nerd = require('./models/nerd');
         });
 
         app.get('/a2drt', function(req, res){
-            res.sendfile('./public/views/index.html')
+            res.sendfile('./public/views/index2.html')
         });
         app.get('/demo', function(req, res) {
             res.sendfile('./public/views/viz.html'); // load our public/index.html file
